a19140018c2e93c28a623d6053e8f7281888041e
ignaciosticco <ignaciosticco@gmail.com>
2024-05-10 10:21:01 -0300

    agrego textloader

--- marketing_ai_app1.py
+++ marketing_ai_app1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain_community.document_loaders import TextLoader
 
 #### Configuracion de la pagina web ####
 
@@ -6,3 +7,6 @@ st.title("Asistente de Marketing virtual")
 st.write("#### Pregunta:")
 pregunta = st.text_input("Hacé tu pregunta")
 st.write("#### Respuesta:")
+
+loader = TextLoader("./prueba.txt", encoding='utf-8')
+text_documents = loader.load()
