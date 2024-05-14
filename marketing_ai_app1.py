fa9cf4ab7e5bd6cfa3e9659d05b8509ef7be2f98
ignaciosticco <ignaciosticco@gmail.com>
2024-05-14 15:26:06 -0300

    cambio la config de powered by

--- marketing_ai_app1.py
+++ marketing_ai_app1.py
@@ -11,14 +11,8 @@ from langchain_openai.chat_models import ChatOpenAI
 
 st.title(":rainbow[Asistente de Marketing virtual]")
 
-col1, col2 = st.columns([1, 6])  # Adjust the ratio as needed
-with col1:
-    st.write("Powered by")
-with col2:
-    st.image("logo_wavebi2.svg", width=80)
-
-#st.write('Powered by')
-#st.image('logo_wavebi2.svg', width=100)
+st.write('Powered by')
+st.image('logo_wavebi2.svg', width=80)
 
 st.write('Este chatbot se presenta como un asistente virtual especializado en marketing. \
          Su función principal radica en la recopilación y análisis de datos provenientes de las campañas de \
